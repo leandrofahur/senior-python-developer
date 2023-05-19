ab8806fee00158faf92208b59b36fcd03fdfd2fd
leandro <fahurleandro@gmail.com>
2023-05-19 14:34:42 -0700

    a few lists methods

---
+++ basics/lists_methods.py
@@ -0,0 +1,24 @@
+# A few lists methods
+
+natural_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print("natural numbers: ", natural_numbers)
+
+# add:
+natural_numbers.append(11)
+print("add: ", natural_numbers)
+
+# remove:
+natural_numbers.remove(11)
+print("remove: ", natural_numbers)
+
+# extend:
+natural_numbers.extend([11, 12, 13, 14, 15])
+print("extend: ", natural_numbers)
+
+# insert:
+natural_numbers.insert(0, 0)
+print("insert: ", natural_numbers)
+
+# pop:
+natural_numbers.pop(0)
+print("pop: ", natural_numbers)
