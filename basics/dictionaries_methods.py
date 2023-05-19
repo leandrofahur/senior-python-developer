9cf71fa4b3785ddfb81f87bae7460f5d245901cd
leandro <fahurleandro@gmail.com>
2023-05-19 14:53:28 -0700

    dictionaries and some access

--- basics/dictionaries_methods.py
+++ basics/dictionaries_methods.py
@@ -5,5 +5,26 @@ dictionary = {
     'd': 4
 }
 
-print(dictionary['a'])
-print(dictionary)
+print("dictionary['a']: ", dictionary['a'])
+print("dictionary: ", dictionary)
+
+my_list = [
+    {
+        'a': 1,
+        'b': 2,
+        'c': 3,
+    },
+    {
+        'a': 4,
+        'b': 5,
+        'c': [6, 7, 8],
+    },
+    {
+        'a': None,
+    }
+]
+
+print("my_list: ", my_list)
+print("my_list[0]: ", my_list[0])
+print("my_list[1]['c']: ", my_list[1]['c'])
+print("my_list[1]['c'][1][1]: ", my_list[1]['c'][1])
