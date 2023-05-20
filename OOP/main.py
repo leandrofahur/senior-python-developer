49f302740c2e5ceff00581ffe94dfca9e78cd1ed
leandro <fahurleandro@gmail.com>
2023-05-20 11:33:25 -0700

    using the @classmethod

--- OOP/main.py
+++ OOP/main.py
@@ -1,14 +1,19 @@
 class PlayerCharacter:
-    def __init__(self, name):
+    def __init__(self, name, age):
         self.name = name
+        self.age = age
 
     def run(self):
         print('run')
 
     def speak(self):
-        print(f'my name is {self.name}')
+        print(f'my name is {self.name} and {self.age} years old')
+
+    @classmethod
+    def adding_things(cls, num_1, num_2):
+        return num_1 + num_2
 
 
-player1 = PlayerCharacter('Cindy')
-print(player1.name)
+player1 = PlayerCharacter('Cindy', 44)
 player1.speak()
+player1.adding_things(2, 3)
