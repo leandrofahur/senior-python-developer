807de6a09ac7071f698f28ee690e9a97f3ee37f4
leandro <fahurleandro@gmail.com>
2023-05-20 11:38:06 -0700

    using the @staticmethod

--- OOP/main.py
+++ OOP/main.py
@@ -7,13 +7,21 @@ class PlayerCharacter:
         print('run')
 
     def speak(self):
+
         print(f'my name is {self.name} and {self.age} years old')
 
     @classmethod
     def adding_things(cls, num_1, num_2):
+        return cls('Tommy', num_1 + num_2)
+
+    @staticmethod
+    def adding_things2(num_1, num_2):
         return num_1 + num_2
 
 
 player1 = PlayerCharacter('Cindy', 44)
 player1.speak()
-player1.adding_things(2, 3)
+
+player2 = PlayerCharacter.adding_things(2, 3)
+print(player2)
+print(PlayerCharacter.adding_things2(2, 3))
