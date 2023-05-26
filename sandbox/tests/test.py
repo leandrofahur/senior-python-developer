2f3b4b998fb9ffa56454949e15d8188a0836359d
leandro <fahurleandro@gmail.com>
2023-05-26 15:44:40 -0700

    beginning with unit test in a non main.py file

--- sandbox/tests/test.py
+++ sandbox/tests/test.py
@@ -1,21 +1,24 @@
 import unittest
-import main
+import script
 
-class TestMain(unittest.TestCase):	
-	def test_do_stuff(self):
-		num = 10
-		result = main.do_stuff(num)
-		self.assertEqual(result, 15)
 
-	def test_do_stuff2(self):
-		test_param = 'asdfdsfsfsd'
-		result = main.do_stuff(test_param)
-		# self.assertTrue(isinstance(result, ValueError))
-		self.assertIsInstance(result, ValueError)
+class TestMain(unittest.TestCase):
+    def test_do_stuff(self):
+        num = 10
+        result = script.do_stuff(num)
+        self.assertEqual(result, 15)
 
-	def test_do_stuff3(self):
-		test_param = None
-		result = main.do_stuff(test_param)
-		self.assertEqual(result, 'please enter a number')
+    def test_do_stuff2(self):
+        test_param = 'asdfdsfsfsd'
+        result = script.do_stuff(test_param)
+        # self.assertTrue(isinstance(result, ValueError))
+        self.assertIsInstance(result, ValueError)
 
-unittest.main()
+    def test_do_stuff3(self):
+        test_param = None
+        result = script.do_stuff(test_param)
+        self.assertEqual(result, 'please enter a number')
+
+
+if __name__ == '__main__':
+    unittest.main()
