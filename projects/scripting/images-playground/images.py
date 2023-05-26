c67b7b5dadda554efdbff9dd0c6aba596c6ee85c
leandro <fahurleandro@gmail.com>
2023-05-26 16:36:03 -0700

    open an image transform it to gray scale

--- projects/scripting/images-playground/images.py
+++ projects/scripting/images-playground/images.py
@@ -1,7 +1,10 @@
 from PIL import Image, ImageFilter
 
 img = Image.open('./Pokedex/pikachu.jpeg')
-filter_image = img.filter(ImageFilter.BLUR)
-
 # print(dir(img))
-filter_image.save('blur_pikachu.png', 'png')
+
+# filter_image = img.filter(ImageFilter.BLUR)
+# filter_image.save('blur_pikachu.png', 'png')
+
+filter_image = img.convert('L')
+filter_image.save('gray_pikachu.png', 'png')
