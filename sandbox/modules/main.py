a4293f495bb342c0ed77c316a12099425f71e297
leandro <fahurleandro@gmail.com>
2023-06-09 10:24:29 -0700

    cleanup for adding all topics related to python. From scripting, to data science

--- sandbox/modules/main.py
+++
@@ -1,5 +0,0 @@
-import utility
-import shopping.shopping_cart as shopping
-
-print(utility.multiply(2, 3))
-print(shopping.buy('apple'))
--- sandbox/modules/utility.py
+++
@@ -1,6 +0,0 @@
-def multiply(x, y):
-    return x * y
-
-
-def divide(x, y):
-    return x / y
